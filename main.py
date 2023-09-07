85569d3c56310f262c4b116dff9042dfb97d15ce
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-09-07 23:28:21 +1000

    Start

---
+++ main.py
@@ -0,0 +1,71 @@
+﻿"""
+n = 5
+print(type(n)) 
+
+n = 'fddf'
+print(type(n))
+
+n = 'fd\'df'
+print(n)
+
+n = 'fd"gbgfb"df'
+print(n)
+"""
+
+# a = 5
+# b = 5.89
+# c = 'hello'
+
+# print(a, b, c)
+# print(f"{a} - {b} - {c}")
+# print("{} - {} - {}".format(a, b, c))
+
+# print('Введите первое число: ')
+# a = input()
+# b = input('Введите второе число: ')
+
+# print(a, ' + ', b, ' = ', a + b)
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = int(c)
+# print(c)
+# print(type(c))
+
+# c = 5.89
+# print(c)
+# print(type(c))
+# c = str(c)
+# print(c)
+# print(type(c))
+
+# c = 5.89
+# print(c + '56')
+# print(type(c))
+# c = str(c)
+# print(c + '89')
+# print(type(c))
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# v = 'fvdfv'
+# v = int(v)
+
+# print('Введите первое число: ')
+# a = int(input())
+# b = int(input('Введите второе число: '))
+
+# print(a, ' + ', b, ' = ', a + b)
+
+# a = 5.89956
+# b = 6.556551
+# print(round(a*b, 3))
+
+name = input("Введите ваше имя: ")  # Запрашивает у пользователя ввод имени
+print("Привет, ", name)             # Выводит приветствие с введенным именем
