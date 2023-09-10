d4ea1af9961082cf263620df1ea2bfb33d6e3cec
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-09-10 22:32:49 +1000

    Checkbox Method

--- main.py
+++ main.py
@@ -87,3 +87,38 @@
 # x = 3.14159
 # rounded = round(x, 2)
 # print(rounded)  # Выведет 3
+
+# a = 1 < 3 < 8 < 10
+# print(a)
+
+# n = 423
+# summa = 0
+# while n > 0:
+#     x = n % 10
+#     summa = summa + x
+#     n = n // 10
+# print(summa)  # 9
+
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         break
+#     i = i + 1
+# else:
+#     print('Пожалуй')
+#     print('хватит')
+# print(i)
+
+# Метод флажка
+
+n = int(input())
+flag = True
+i = 2
+while flag:
+    if n % i == 0:  # если остаток при делении чисел n на i равен 0
+        flag = False
+        print(i)
+    elif i > n // 2:  # делить числа не может превышать введенное число, деленное на 2
+        print(n)
+        flag = False
+    i += 1
