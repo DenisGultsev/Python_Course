54ed87768333bcaea9e3af51d3acac84b5336fd7
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-09-10 22:42:08 +1000

    Cycles

--- main.py
+++ main.py
@@ -111,14 +111,31 @@
 
 # Метод флажка
 
-n = int(input())
-flag = True
-i = 2
-while flag:
-    if n % i == 0:  # если остаток при делении чисел n на i равен 0
-        flag = False
-        print(i)
-    elif i > n // 2:  # делить числа не может превышать введенное число, деленное на 2
-        print(n)
-        flag = False
-    i += 1
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0:  # если остаток при делении чисел n на i равен 0
+#         flag = False
+#         print(i)
+#     elif i > n // 2:  # делить числа не может превышать введенное число, деленное на 2
+#         print(n)
+#         flag = False
+#     i += 1
+
+# Цикл for, функция range()
+
+# a = 'qwerty'
+
+# print(a[0])
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
+
+line = ""
+for i in range(5):
+    line = ""
+    for j in range(5):
+        line += "*"
+    print(line)
