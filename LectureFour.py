e09aa5f15d920c438b7c8bdba6539ed7a3ccf914
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-11 23:58:52 +1000

    Lambda

---
+++ LectureFour.py
@@ -0,0 +1,38 @@
+﻿# def f(x):
+#     return x ** 2
+
+
+# print(f(4))  # 16
+# print(type(f))  # <class 'function'>
+
+# def f(x):
+#     return x ** 2
+
+
+# a = f
+# print(a(4))  # 16
+# print(type(a))  # <class 'function'>
+
+# def calc1(x):
+#     return x + x
+
+
+# def calc2(x):
+#     return x * x
+
+
+# def math(op, x):
+#     print(op(x))
+
+
+# math(calc2, 5)
+
+# def calk1(a, b):
+#     return a * b
+
+
+# def math(op, x, y):
+#     print(op(x, y))
+
+
+# math(lambda a, b: a + b, 5, 45)
