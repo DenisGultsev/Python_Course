4772f32ce756eb2121f1c31be7081d46009cebfc
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-12 23:57:19 +1000

    Map

--- LectureFour.py
+++ LectureFour.py
@@ -44,18 +44,43 @@
 #         out.append((i, i ** 2))
 # print(out)
 
-def select(f, col):
-    return [f(x) for x in col]
+# def select(f, col):
+#     return [f(x) for x in col]
 
+
+# def where(f, col):
+#     return [x for x in col if f(x)]
+
+
+# data = [1, 2, 3, 5, 8, 15, 23, 38]
+# res = select(int, data)
+# print(res)
+# res = where(lambda x: x % 2 == 0, res)
+# print(res)  # [2, 8, 38]
+# res = list(select(lambda x: (x, x ** 2), res))
+# print(res)
+
+# list_1 = [x for x in range(1, 20)]
+# print(list_1)
+
+# list_1 = list(map(lambda x: x + 10, list_1))
+# print(list_1)
+
+# C клавиатуры вводится некий набор чисел, в качестве разделителя
+# используется пробел. Этот набор чисел будет считан в качестве строки. Как
+# превратить list строк в list чисел?
+
+# data = '15 156 96 3 5 8 52 5'
+
+# data = list(map(int, data.split()))
+# print(data)
 
 def where(f, col):
     return [x for x in col if f(x)]
 
 
-data = [1, 2, 3, 5, 8, 15, 23, 38]
-res = select(int, data)
-print(res)
+data = '1 2 3 5 8 15 23 38'.split()
+res = map(int, data)
 res = where(lambda x: x % 2 == 0, res)
-print(res)  # [2, 8, 38]
-res = list(select(lambda x: (x, x ** 2), res))
+res = list(map(lambda x: (x, x ** 2), res))
 print(res)
