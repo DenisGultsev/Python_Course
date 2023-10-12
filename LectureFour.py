cec7bcb23a9eba2d9348be8d80a4b23799cb6faa
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-13 00:28:54 +1000

    Zip

--- LectureFour.py
+++ LectureFour.py
@@ -93,8 +93,22 @@
 
 # filter() позволит избавиться от функции where, которую мы писали ранее
 
-data = '1 2 3 5 8 15 23 38'.split()
-res = map(int, data)
-res = filter(lambda x: x % 2 == 0, res)
-res = list(map(lambda x: (x, x ** 2), res))
-print(res)
+# data = '1 2 3 5 8 15 23 38'.split()
+# res = map(int, data)
+# res = filter(lambda x: x % 2 == 0, res)
+# res = list(map(lambda x: (x, x ** 2), res))
+# print(res)
+
+# Фуункция zip
+
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# data = list(zip(users, ids))
+# # [('user1', 4), ('user2', 5), ('user3', 9), ('user4', 14), ('user5', 7)]
+# print(data)
+
+# users = ['user1', 'user2', 'user3', 'user4', 'user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111, 222, 333]
+# data = list(zip(users, ids, salary))
+# print(data)  # [('user1', 4, 111), ('user2', 5, 222), ('user3', 333)]
