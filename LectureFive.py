d1a11e234096dc242b6c525ffe743da6a391910a
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-17 01:59:23 +1000

    Google Colab

--- LectureFive.py
+++ LectureFive.py
@@ -1,1 +1,49 @@
-﻿
+﻿# import matplotlib.pyplot as plt
+
+# # Пример данных
+# x = [6, 2, 3, 4, 5]
+# y = [2, 4, 6, 8, 10]
+
+# # Создание точечного графика
+# plt.scatter(x, y)
+
+# # Добавление подписей и заголовка
+# plt.xlabel('Ось X')
+# plt.ylabel('Ось Y')
+# plt.title('Точечный график')
+
+# # Отображение графика
+# plt.show()
+
+# def find_median(nums):
+#     nums.sort()
+#     n = len(nums)
+#     if n % 2 == 0:
+#         mid1 = nums[n // 2]
+#         mid2 = nums[n // 2 - 1]
+#         median = (mid1 + mid2) / 2
+#     else:
+#         median = nums[n // 2]
+#     return median
+
+
+# # Пример использования функции
+# numbers = [5, 3, 1, 2, 4]
+# median = find_median(numbers)
+# print("Медиана:", median)
+
+# import numpy as np
+
+# # Создание массива с помощью NumPy
+# arr = np.array([1, 2, 3, 4, 5])
+
+# # Использование функций NumPy
+# mean = np.mean(arr)
+# median = np.median(arr)
+# std = np.std(arr)
+
+# print("Среднее значение:", mean)
+# print("Медиана:", median)
+# print("Стандартное отклонение:", std)
+
+import pandas as pd
