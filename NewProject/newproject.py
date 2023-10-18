1030282a6456f91fff6ee7051954d9f4e45632f2
DenisGultsev <135717609+DenisGultsev@users.noreply.github.com>
2023-10-19 01:41:56 +1000

    Fix

--- NewProject/newproject.py
+++ NewProject/newproject.py
@@ -1,16 +1,1 @@
-﻿def find_median(nums):
-    nums.sort()
-    n = len(nums)
-    if n % 2 == 0:
-        mid1 = nums[n // 2]
-        mid2 = nums[n // 2 - 1]
-        median = (mid1 + mid2) / 2
-    else:
-        median = nums[n // 2]
-    return median
-
-
-# Пример использования функции
-numbers = [5, 3, 1, 2, 4]
-median = find_median(numbers)
-print("Медиана:", median)
+﻿
